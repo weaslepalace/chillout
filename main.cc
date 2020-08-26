2ced7c381185bd5e2db3d28127fef164992f831f
Christian <christian@simplglobal.com>
2020-08-26 13:02:11 -0700

    It is very important that the Gtk app is created before instantiating objects that depend on Gtk. Move the call to Gtk::Application::create() to be top of main(). Fixed segfaults

--- main.cc
+++ main.cc
@@ -4,11 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-	ChillOut chill_out;
 
 	auto app = Gtk::Application::create(
 		argc,
 		argv,
 		"org.weaslepalace.chillout");
+
+	ChillOut chill_out;
+
 	return app->run(*chill_out.front_panel.win);
 }
